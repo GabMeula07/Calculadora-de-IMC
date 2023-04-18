b8a5aaa5b64c7ffe39989bb826358ec0bdbc7f2f
GabiMeula <gabrielqueirozmeula@gmail.com>
2023-04-18 13:11:00 -0300

    Projeto Finalizado

---
+++ IMC/assets/js/main.js
@@ -0,0 +1,84 @@
+function meuEscopo() {
+  const form = document.querySelector(".form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const inputPeso = e.target.querySelector("#peso");
+    const inputAltura = e.target.querySelector("#altura");
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+    if (!peso) {
+      setResultado("Peso invalido", false);
+      return;
+    }
+    if (!altura) {
+      setResultado("Altura invalida", false);
+      return;
+    }
+    if(altura > 2.50){
+      setResultado("Altura invalida", false);
+      return;
+    }
+    if(peso > 595){
+      setResultado("Peso Invalido", false)
+      return
+    }
+
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+    setResultado(`Seu imc é ${imc} e você está no categoria ${nivelImc}`, true)
+  });
+
+  function getNivelImc(imc) {
+    const nivel = [
+      "Abaixo do peso",
+      "Peso normal",
+      "Sobrepeso",
+      "Obseidade grau 1",
+      "Obesidade grau 2",
+      "Obesidade grau 3",
+    ];
+
+    if (imc >= 39.9){
+        return nivel[5];
+    } 
+    if (imc >= 34.9){
+        return nivel[4];
+    }  
+    if (imc >= 29.9){
+        return nivel[3];
+    }  
+    if (imc >= 24.9){
+        return nivel[2];
+    }  
+    if (imc >= 18.5){
+        return nivel[1];
+    }  
+    if (imc < 18.5){
+        return nivel[0];
+    }
+
+  }
+  function getImc(peso, altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+  }
+
+  function criaP() {
+    const p = document.createElement("p");
+    return p;
+  }
+  function setResultado(msg, isValid) {
+    const resultado = document.querySelector(".res");
+    resultado.innerHTML = "";
+    const p = criaP();
+    if (isValid){
+        p.classList.add('paragrafo-resultado')
+    } else{
+      p.classList.add('bad')
+    }
+    p.innerHTML = msg;
+    resultado.appendChild(p);
+  }
+}
+meuEscopo();
